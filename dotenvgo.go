383e45c3cf9c83fcc7a4f7fa508eb463a9a80ed5
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-08-18 20:18:14 +0300

    make it more general

--- dotenvgo.go
+++ dotenvgo.go
@@ -2,117 +2,46 @@ package dotenvgo
 
 import (
 	"log"
-	"net/url"
 	"os"
 	"path/filepath"
-	"strconv"
+	"regexp"
 	"strings"
 )
 
-type Env struct {
-	Env           string
-	Domain        string
-	DBService     string
-	DB            string
-	DBUser        string
-	DBPassword    string
-	DBPort        int
-	AdminHost     *url.URL
-	APIHost       *url.URL
-	AuthHost      *url.URL
-	AuthClientID  string
-	Secret        string
-	Salt          string
-	Port          int
-	EmailSMTP     string
-	EmailAddress  string
-	EmailUsername string
-	EmailPassword string
-	EmailPort     int
+var vars map[string]string = map[string]string{}
+
+func Get(variable string) *string {
+	v, ok := vars[variable]
+	if ok {
+		return &v
+	}
+	return nil
 }
 
-func NewEnv() *Env {
-	env := new(Env)
+func Load(path *string) {
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	pathToEnv := filepath.Join(dir, ".env")
+	p := ".env"
+	if path != nil {
+		p = *path
+	}
+	pathToEnv := filepath.Join(dir, p)
 	file, err := os.ReadFile(pathToEnv)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	regex := regexp.MustCompile(`^(\w*)=['"]?([^'"]+?)['"]?$`)
 
 	lines := strings.Split(string(file), "\n")
 	for _, line := range lines {
-		if len(line) != 0 && !strings.HasPrefix(line, "#") {
-			pair := strings.Split(line, "=")
-			trimmed := strings.Trim(strings.Trim(pair[1], `"`), `'`)
-
-			switch strings.ToLower(pair[0]) {
-			case "env":
-				if len(trimmed) != 0 {
-					env.Env = trimmed
-				} else {
-					env.Env = "release"
-				}
-			case "domain":
-				env.Domain = trimmed
-			case "db":
-				env.DB = trimmed
-			case "db_service":
-				env.DBService = trimmed
-			case "db_user":
-				env.DBUser = trimmed
-			case "db_password":
-				env.DBPassword = trimmed
-			case "port":
-				if port, err := strconv.Atoi(trimmed); err == nil {
-					env.Port = port
-				} else {
-					env.Port = 8080
-				}
-			case "db_port":
-				if port, err := strconv.Atoi(trimmed); err == nil {
-					env.DBPort = port
-				} else {
-					env.DBPort = 5432
-				}
-			case "admin_host":
-				if u, err := url.Parse(trimmed); err == nil {
-					env.AdminHost = u
-				}
-			case "api_host":
-				if u, err := url.Parse(trimmed); err == nil {
-					env.APIHost = u
-				}
-			case "auth_host":
-				if u, err := url.Parse(trimmed); err == nil {
-					env.AuthHost = u
-				}
-			case "auth_client_id":
-				env.AuthClientID = trimmed
-			case "secret":
-				env.Secret = trimmed
-			case "salt":
-				env.Salt = trimmed
-			case "email_smtp":
-				env.EmailSMTP = trimmed
-			case "email_address":
-				env.EmailAddress = trimmed
-			case "email_user":
-				env.EmailUsername = trimmed
-			case "email_password":
-				env.EmailPassword = trimmed
-			case "email_port":
-				if port, err := strconv.Atoi(trimmed); err == nil {
-					env.EmailPort = port
-				} else {
-					env.EmailPort = 587
-				}
+		if len(line) != 0 {
+			match := regex.FindStringSubmatch(line)
+			if len(match) == 3 {
+				vars[match[1]] = string(match[2])
 			}
 		}
 	}
-	return env
 }
+
